babe4705dafde6863de104a17303a3a4bd356448
AW <djwotmd@gmail.com>
2022-11-11 17:42:50 +0900

    add Intro at hOMe

--- logossoft/src/pages/Home.js
+++ logossoft/src/pages/Home.js
@@ -1,10 +1,11 @@
 import TopNav from '../components/TopNav.js';
+import MainBanner from '../components/MainBanner.js';
 
 export default function Home({target}) {
   
   this.render = () => {
     new TopNav({ target, initState: { menu: "home"} });
-
+    new MainBanner({ target });
   }
   this.render();
 }
