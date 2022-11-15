19529e87052f553a7699a45070db5c7f7c0d6283
AW <djwotmd@gmail.com>
2022-11-15 10:24:07 +0900

    make Store

--- logossoft/index.js
+++ logossoft/index.js
@@ -1,3 +1,24 @@
 import App from './src/App.js';
+import Store from './src/utils/Store.js';
 
 new App({ target: document.querySelector(".app") });
+
+const initState = { recentlyViewedList: [] };
+const reducer = (state, actionKey, { text }) => {
+  const { recentlyViewedList } = state;
+
+  switch (actionKey) {
+    case "ADD_ITEM":
+      const newList = [text, ...recentlyViewedList.filter((v) => v !== text)];
+      return { ...state, recentlyViewedList: newList };
+    default:
+      return { ...state };
+  }
+};
+const a = new Store(initState, reducer);
+const subFunc = () => {
+  console.log("읽었지요")
+  console.log(a.getState());
+}
+a.subscribe(subFunc);
+a.dispatch("ADD_ITEM", { text: "디스패치이" });
--- logossoft/src/utils/Store.js
+++ logossoft/src/utils/Store.js
@@ -1,6 +1,6 @@
 export default function Store(state = {}, reducer) {
   this.state = state;
-  this.listener = [];
+  this.listeners = [];
   this.reducer = reducer;
 
   this.getState = () => {
@@ -20,20 +20,17 @@ export default function Store(state = {}, reducer) {
     this.publish();
   }
 
-  //https://velog.io/@bepyan/Vanilla-JS-Flux-%ED%8C%A8%ED%84%B4%EC%9C%BC%EB%A1%9C-%EC%83%81%ED%83%9C-%EA%B4%80%EB%A6%AC%ED%95%98%EA%B8%B0-feat.-Store
-
-  // return new Proxy(data, {
-	// 	get: function (obj, prop) {
-	// 		return obj[prop];
-	// 	},
-	// 	set: function (obj, prop, value) {
-	// 		if (obj[prop] === value) return true;
-	// 		obj[prop] = value;
-	// 		return true;
-	// 	},
-	// 	deleteProperty: function (obj, prop) {
-	// 		delete obj[prop];
-	// 		return true;
-	// 	}
-	// });
 }
+
+const initState = { recentlyViewedList: [] };
+const reducer = (state, actionKey, { text }) => {
+  const { recentlyViewedList } = state;
+
+  switch (actionKey) {
+    case "ADD_ITEM":
+      const newList = [text, ...recentlyViewedList.filter((v) => v !== text)];
+      return { ...state, recentlyViewedList: newList };
+    default:
+      return { ...state };
+  }
+};
