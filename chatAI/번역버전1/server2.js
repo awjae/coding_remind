5cd306ff7e302687d7aaa36a99e58f3e3c3aa3c2
awjae <djwotmd@gmail.com>
2022-11-29 23:11:12 +0900

    translate api + open ai api

--- chatAI/번역버전1/server2.js
+++ chatAI/번역버전1/server2.js
@@ -1,27 +1,80 @@
 // 네이버 기계번역 API 예제
-var express = require('express');
-var app = express();
-var client_id = 'jTY09BfHE8xRZMUP8DS3401';
-var client_secret = 'JGFOpJbJ2W401';
-var query = "번역할 문장을 입력하세요.";
+const express = require('express');
+const app = express();
+const { OpenAIApi, Configuration } = require('openai');
+
+let config = new Configuration({
+  apiKey: 'sk-dVa4D0gwsO6BcZIwf6cmT3BlbkFJxf1mF1kGODhA83e6xLgA401',
+});
+let openai = new OpenAIApi(config);
+
+const client_id = 'jTY09BfHE8xRZMUP8DS3424';
+const client_secret = 'JGFOpJbJ2W495';
+
 app.get('/translate', function (req, res) {
-   var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
-   var request = require('request');
-   var options = {
-       url: api_url,
-       form: {'source':'ko', 'target':'en', 'text':query},
-       headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
-    };
-   request.post(options, function (error, response, body) {
-     if (!error && response.statusCode == 200) {
-       res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-       res.end(body);
-     } else {
-       res.status(response.statusCode).end();
-       console.log('error = ' + response.statusCode);
-     }
-   });
- });
- app.listen(3000, function () {
-   console.log('http://127.0.0.1:3000/translate app listening on port 3000!');
- });
+  const query = req.query.q;
+  const api_url = 'https://openapi.naver.com/v1/papago/n2mt';
+  const request = require('request');
+  const options = {
+    url: api_url,
+    form: {
+      'source': 'ko',
+      'target': 'en',
+      'text': query
+    },
+    headers: {
+      'X-Naver-Client-Id': client_id,
+      'X-Naver-Client-Secret': client_secret
+    }
+  };
+  request.post(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      let EN_TEXT = JSON.parse(body).message?.result.translatedText;
+      openai.createCompletion({
+        model: "text-davinci-002",
+        prompt: EN_TEXT,
+        temperature: 0.7,
+        max_tokens: 128,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      }).then((result) => {
+        console.log('ai 응답', result.data.choices[0].text);
+
+        const api_url = 'https://openapi.naver.com/v1/papago/n2mt';
+        const query = result.data.choices[0].text;
+        const options = {
+          url: api_url,
+          form: {
+            'source': 'en',
+            'target': 'ko',
+            'text': query
+          },
+          headers: {
+            'X-Naver-Client-Id': client_id,
+            'X-Naver-Client-Secret': client_secret
+          }
+        };
+        request.post(options, function (error, response, body) {
+          console.log(body);
+          // res.writeHead(200, {
+          //   'Content-Type': 'text/json;charset=utf-8'
+          // });
+          res.status(200).json(body);
+          // res.end();
+        });
+      }).catch((error) => {
+        console.log('openai error', error)
+      })
+    } else {
+      res.status(response.statusCode).end();
+      console.log('error = ' + response.statusCode);
+    }
+  });
+});
+app.listen(3000, function () {
+  console.log('http://127.0.0.1:3000/translate app listening on port 3000!');
+});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
