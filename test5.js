89b80226dd52f893629070ba6dfb29e367fa994c
AW <djwotmd@gmail.com>
2023-03-10 15:39:18 +0900

    [ADD] : userAgent Validation 추가

---
+++ test5.js
@@ -0,0 +1,17 @@
+const [a, b] = [1, 2];
+console.log(a, b);
+
+
+const { title } = { title: "asd", item: "mask"};
+console.log(title); 
+
+const c = "1";
+const d = 2321;
+
+const object = {
+  a,
+  b,
+  c,
+  d
+}
+console.dir(object);
